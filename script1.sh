11d4a87a40d7884db27182eaa5c21a8910f58524
shubh gupta <sept1st2c@gmail.com>
2024-08-28 09:35:40 +0530

    learning indexes

--- script1.sh
+++ script1.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 name=Pinku
-
-read -p "enter agei: " age
+age = $1
+#read -p "enter agei: " age
 echo "My name is $name.My age is $age. Today's date is $(date)"
+echo "this is the second index: $2"
+echo "3rd index: $3"
